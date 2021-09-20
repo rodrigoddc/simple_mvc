c30bd1bd422e738a24a673a603b9729b089d8a22
Edu <edu.mangaba@gmail.com>
2021-09-19 21:16:21 -0300

    feat: settings by pydantic + dotenv

--- app_edu/setup/settings.py
+++ app_edu/setup/settings.py
@@ -1,3 +1,11 @@
-import os
+from pydantic import BaseSettings, Field
 
-tmdb_api_key = os.getenv("TMDB_API_KEY")
+
+class Settings(BaseSettings):
+    app_name: str = "App do Edu"
+    tmdb_api_key: str = Field(env='TMDB_API_KEY')
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
+tmdb_api_key = settings.tmdb_api_key
--- app_edu/src/controllers/tmdb_requests.py
+++ app_edu/src/controllers/tmdb_requests.py
@@ -2,6 +2,7 @@ import requests
 from setup.settings import tmdb_api_key
 
 
+
 def retrieve_movie_info_message(movie_id: str):
     movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={tmdb_api_key}"
     response = requests.get(movie_url).json()
