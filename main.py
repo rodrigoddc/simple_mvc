34494c3c633c75d6882cc0b338c30cd865f12521
Rodrigo Azevedo da Silva <azevedo@MacBook-de-Rodrigo.local>
2021-09-26 16:18:47 -0300

    add parameters to FastAPI

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from fastapi import FastAPI
 
 from src.controller.controller import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Simple MVC API",
+    contact={"name": "Rodrigo Azevedo", "e-mail": "razevedo.contato@gmail.com"},
+)
 app.include_router(router=router)
