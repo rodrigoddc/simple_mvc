5426827b22a1a2cd30c66de9230f82d9d04e45cd
razevedo1994 <razevedo.contato@gmail.com>
2021-11-24 23:57:07 -0300

    update project

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
-from src.movie.controller.movie_controller import router
+from src.movie.controllers.movie_rating_controller import movie_rating_router
+from src.movie.controllers.movie_details_controller import movie_detail_router
+from src.exceptions.exception_handler.movie_exception_handlers import movie_exception_handler, \
+    movie_not_found_exception_handler, movie_missing_data_exception_handler
+from src.exceptions.movie_exceptions import MovieRatingException, MovieNotFoundException, \
+    MovieMissingDataFromTMDBAPIException
 
-app = FastAPI(
-    title="Simple MVC API",
-    contact={"name": "Rodrigo Azevedo", "e-mail": "razevedo.contato@gmail.com"},
-)
-app.include_router(router=router)
+app = FastAPI()
+app.include_router(router=movie_rating_router)
+app.include_router(router=movie_detail_router)
+app.add_exception_handler(MovieRatingException, movie_exception_handler)
+app.add_exception_handler(MovieNotFoundException, movie_not_found_exception_handler)
+app.add_exception_handler(MovieMissingDataFromTMDBAPIException, movie_missing_data_exception_handler)
